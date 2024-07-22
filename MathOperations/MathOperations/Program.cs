b18e938d7755057ddee0fced867a4775f30e520d
axelpxl33 <akselnordevpxl33@gmail.com>
2024-07-22 16:44:35 +0300

    Project MathOperations

---
+++ MathOperations/MathOperations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class MathOperations<T>
+{
+   public T Add(T a, T b)
+    {
+        dynamic A = a;
+        dynamic B = b;
+        return A + B;
+    }
+
+    public T Substract(T a, T b)
+    {
+        dynamic A = a;
+        dynamic B = b;
+        return A - B;
+    }
+    
+    public T Multiply(T a, T b)
+    {
+        dynamic A = a;
+        dynamic B = b;
+        return A * B;
+    }
+
+    public T Divide(T a, T b)
+    {
+        dynamic A = a;
+        dynamic B = b;
+        return A / B;
+
+        if (B == 0)
+        {
+            throw new ArgumentException("Impartirea la 0 nu exista");
+        }
+        return A / B;
+    }
+}
